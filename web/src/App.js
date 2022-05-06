552c29188f88b81a09d1fd3454a6bdb93f741b66
NachitoPerez <irperez@fi.uba.ar>
2022-05-06 09:48:51 -0300

    pagina inicial de login con firebase

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,63 @@
-import './App.css';
-import UsersTable from "./components/UsersTable";
-
+//import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 function App() {
-  return <UsersTable />
+    return (
+        <div className="app">
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Login />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
-
 export default App;
+
+
+{/*import
+    './App.css';
+import
+    UsersTable
+    from
+    "./components/UsersTable";
+import
+    LogInPage
+    from
+    "./components/LogInPage";
+import
+    {
+        useState
+    }
+    from
+    "react";
+import
+    {
+        BrowserRouter
+        as
+        Router, Route, Routes
+    }
+    from
+    "react-router-dom";
+import
+    Login
+    from
+    "./components/Login";
+
+    function App() {
+        {
+        const [logged, setLogged] = useState(false);
+        if (logged) {
+            return <UsersTable/>
+        } else {
+            return <LogInPage logIn={setLogged}/>
+        }
+
+        }
+
+        return <Login/>
+    }
+
+export default
+    App;
+*/}
