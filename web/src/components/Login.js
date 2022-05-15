5ba0b3a2f53e79aa81a864ae24fc8fbab5d74fee
NachitoPerez <irperez@fi.uba.ar>
2022-05-15 17:18:52 -0300

    se valida si el user loggeado es admin

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { auth, logInWithEmailAndPassword } from "./../firebase";
-import {onAuthStateChanged } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {auth, logInWithEmailAndPassword} from "./../firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
 import "./Login.css";
+import {signOut} from "firebase/auth";
 
 function Login({setLoggedIn, setToken}) {
     const [email, setEmail] = useState("");
@@ -16,13 +16,61 @@ function Login({setLoggedIn, setToken}) {
             return;
         }
         if (user) {
-            setToken(user.accessToken);
-            setLoggedIn(true);
-            navigate("/dashboard");
+            /*try {
+                validateAdmin(user.getIdToken())//refresca el token si expiro
+                    .then((status) => {
+                        if (status === 200) {
+                            setToken(user);
+                            setLoggedIn(true);
+                            navigate("/dashboard");
+                        } else {
+                            //mostrar pantalla de "acceso no disponible a no-admin-users"
+                            console.log("No es admin");
+                            console.log("status: ", response.status);
+                            useNavigate("/");
+                        }
+                }).catch((error) => {
+                    console.log(error);
+                });
+            } catch(e) {
+                console.log(e);
+            }*/
+            user.getIdToken()//refresca el token si expiro
+                .then((token) => {
+                    fetch("https://fiubify-middleware-staging.herokuapp.com/auth/validate/admin", {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: {
+                            token: token
+                        }
+                    }).then((response) => {
+                        if (response.status === 200) {
+                            setToken(user);
+                            setLoggedIn(true);
+                            navigate("/dashboard");
+                        } else {
+                            //mostrar pantalla de "acceso no disponible a no-admin-users"
+                            signOut(auth).then(()=>{
+                                console.log("No es admin");
+                                console.log("status: ", response.status);
+                                navigate("/");
+                            }).catch((error)=>{
+                                console.log(error);
+                            })
+                        }
+                    })
+                        .catch((error) => {
+                            console.log(error);
+                        })
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+
         }
     }, [user, loading]);
-
-
     return (
         <div className="login">
             <div className="login__container">
