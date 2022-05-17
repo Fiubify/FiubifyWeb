9d5bb2a836c4c13606eb1e60530acf5b6f00c3f8
NachitoPerez <irperez@fi.uba.ar>
2022-05-17 19:04:35 -0300

    feat: se cierra el popup al apretar el close-btn

--- web/src/components/ErrPopup.js
+++ web/src/components/ErrPopup.js
@@ -1,17 +1,19 @@
 import './ErrPopup.css'
 import {useNavigate} from "react-router-dom";
 
-function ErrPopup({trigger, status, message, dest}) {
+function ErrPopup({trigger, setTrigger, status, message, dest}) {
     const navigate = useNavigate();
-    return (trigger) ? (
-        <div className="popup">
+    return (trigger) ? (<div className="popup">
             <div className="popup-inner">
                 <h3>Error {status}</h3>
                 <p>{message}</p>
-                <button className="close-btn" onClick={()=> {navigate(dest)}}>Received</button>
+                <button className="close-btn" onClick={() => {
+                    setTrigger(false);
+                    navigate(dest);
+                }}>Received
+                </button>
             </div>
-        </div>
-    ) : "";
+        </div>) : "";
 }
 
 export default ErrPopup;
