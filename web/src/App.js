ae1f91498d47e35ed97e7ec41bef2d7e817aa8ae
NachitoPerez <irperez@fi.uba.ar>
2022-05-18 20:41:03 -0300

    refactor: se eliminan comentarios

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,3 @@
-//import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import UsersTable from "./components/UsersTable";
@@ -6,7 +5,7 @@ import {useState} from "react";
 import LoggedInChecker from "./components/LoggedInChecker";
 function App() {
     const [loggedIn, setLoggedIn] = useState(false);
-    const [token, setToken] = useState('');
+    const [_token, setToken] = useState('');
 
     return (
         <div className="app">
@@ -20,51 +19,3 @@ function App() {
     );
 }
 export default App;
-
-
-{/*import
-    './App.css';
-import
-    UsersTable
-    from
-    "./components/UsersTable";
-import
-    LogInPage
-    from
-    "./components/LogInPage";
-import
-    {
-        useState
-    }
-    from
-    "react";
-import
-    {
-        BrowserRouter
-        as
-        Router, Route, Routes
-    }
-    from
-    "react-router-dom";
-import
-    Login
-    from
-    "./components/Login";
-
-    function App() {
-        {
-        const [logged, setLogged] = useState(false);
-        if (logged) {
-            return <UsersTable/>
-        } else {
-            return <LogInPage logIn={setLogged}/>
-        }
-
-        }
-
-        return <Login/>
-    }
-
-export default
-    App;
-*/}
