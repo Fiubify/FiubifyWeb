7e223f2321c44be1906b75c9a2eb7c0fb309f011
NachitoPerez <irperez@fi.uba.ar>
2022-05-19 15:12:56 -0300

    feat: _id clicked turns on ProfilePopup

--- web/src/components/Users/UserRow.js
+++ web/src/components/Users/UserRow.js
@@ -1,47 +1,50 @@
-import { TableCell, TableRow, Chip } from '@mui/material';
+import {TableCell, TableRow, Chip} from '@mui/material';
 import ButtonWithLoading from '../Buttons/ButtonWithLoading';
 
 import PropTypes from 'prop-types';
 
-export default function UserRow({ user, handleBlockUser, handleUnblockUser }) {
-  const { _id, uid, email, role, disabled } = user;
-  const buttonText = disabled ? 'Unblock' : 'Block';
-  const buttonAction = disabled ? handleUnblockUser : handleBlockUser;
+export default function UserRow({user, handleBlockUser, handleUnblockUser, turnOnPopup, setUserProfile}) {
+    const {_id, uid, email, role, disabled} = user;
+    const buttonText = disabled ? 'Unblock' : 'Block';
+    const buttonAction = disabled ? handleUnblockUser : handleBlockUser;
 
-  return (
-    <TableRow>
-      <TableCell>{_id}</TableCell>
-      <TableCell>{uid}</TableCell>
-      <TableCell>{email}</TableCell>
-      <TableCell>
-        <Chip color='info' label={role}></Chip>
-      </TableCell>
-      <TableCell role={'status'}>
-        <Chip
-          color={disabled ? 'error' : 'success'}
-          label={String(disabled).toUpperCase()}
-        />
-      </TableCell>
-      <TableCell align='center'>
-        <ButtonWithLoading
-          variant={'contained'}
-          onClickHandler={() => buttonAction(_id)}
-        >
-          {buttonText}
-        </ButtonWithLoading>
-      </TableCell>
-    </TableRow>
-  );
+    return (
+        <TableRow>
+            <TableCell><p onClick={() => {
+                turnOnPopup(true);
+                setUserProfile(user);
+            }}>{_id}</p></TableCell>
+            <TableCell>{uid}</TableCell>
+            <TableCell>{email}</TableCell>
+            <TableCell>
+                <Chip color='info' label={role}></Chip>
+            </TableCell>
+            <TableCell role={'status'}>
+                <Chip
+                    color={disabled ? 'error' : 'success'}
+                    label={String(disabled).toUpperCase()}
+                />
+            </TableCell>
+            <TableCell align='center'>
+                <ButtonWithLoading
+                    variant={'contained'}
+                    onClickHandler={() => buttonAction(_id)}
+                >
+                    {buttonText}
+                </ButtonWithLoading>
+            </TableCell>
+        </TableRow>
+    );
 }
 
 UserRow.propTypes = {
-  user: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    uid: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    role: PropTypes.string.isRequired,
-    disabled: PropTypes.bool.isRequired,
-  }).isRequired,
-  handleBlockUser: PropTypes.func.isRequired,
-  handleUnblockUser: PropTypes.func.isRequired,
+    user: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        uid: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        disabled: PropTypes.bool.isRequired,
+    }).isRequired,
+    handleBlockUser: PropTypes.func.isRequired,
+    handleUnblockUser: PropTypes.func.isRequired,
 };
--- web/src/components/Users/UsersTable.js
+++ web/src/components/Users/UsersTable.js
@@ -1,58 +1,54 @@
 import {
-  TableBody,
-  TableCell,
-  Table,
-  TableContainer,
-  TableHead,
-  TableRow,
+    TableBody, TableCell, Table, TableContainer, TableHead, TableRow,
 } from '@mui/material';
 import UserRow from './UserRow';
 
 import PropTypes from 'prop-types';
+import ProfilePopup from "../Popup/ProfilePopup";
+import {useState} from "react";
 
 export default function UsersTable({
-  users,
-  handleBlockUser,
-  handleUnblockUser,
-}) {
-  return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell>Uid</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Role</TableCell>
-            <TableCell>Blocked?</TableCell>
-            <TableCell align='center'>Operation</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((user) => (
-            <UserRow
-              key={user._id}
-              user={user}
-              handleBlockUser={handleBlockUser}
-              handleUnblockUser={handleUnblockUser}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+                                       users, handleBlockUser, handleUnblockUser,
+                                   }) {
+    const [popupOn, setPopupOn] = useState(false);
+    const [userProfile, setUserProfile] = useState(null);
+    return (<div>
+        <div>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Id</TableCell>
+                            <TableCell>Uid</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell>Role</TableCell>
+                            <TableCell>Blocked?</TableCell>
+                            <TableCell align='center'>Operation</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users.map((user) => (<UserRow
+                            key={user._id}
+                            user={user}
+                            handleBlockUser={handleBlockUser}
+                            handleUnblockUser={handleUnblockUser}
+                            turnOnPopup={setPopupOn}
+                            setUserProfile={setUserProfile}
+                        />))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+        <ProfilePopup trigger={popupOn} setTrigger={setPopupOn} user={userProfile}></ProfilePopup>
+    </div>);
 }
 
 UsersTable.propTypes = {
-  users: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      uid: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      role: PropTypes.string.isRequired,
-      disabled: PropTypes.bool.isRequired,
-    }).isRequired
-  ).isRequired,
-  handleBlockUser: PropTypes.func.isRequired,
-  handleUnblockUser: PropTypes.func.isRequired,
+    users: PropTypes.arrayOf(PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        uid: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        role: PropTypes.string.isRequired,
+        disabled: PropTypes.bool.isRequired,
+    }).isRequired).isRequired, handleBlockUser: PropTypes.func.isRequired, handleUnblockUser: PropTypes.func.isRequired,
 };
