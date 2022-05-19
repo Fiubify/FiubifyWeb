ad7310a3b754884aabe62bb54fbb643aa606124f
NachitoPerez <irperez@fi.uba.ar>
2022-05-19 12:09:30 -0300

    refactor: se eliminan funciones importadas de firebase sin usar

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut,} from "firebase/auth";
+import {getAuth, signInWithEmailAndPassword, sendPasswordResetEmail, signOut,} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
