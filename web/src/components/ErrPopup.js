15c87ddddf25011058f2ebbb2cad7c6776e3f64c
NachitoPerez <irperez@fi.uba.ar>
2022-05-18 22:58:16 -0300

    fix: al cerrar el popup, se vuelve a la pagina anterior

--- web/src/components/ErrPopup.js
+++ web/src/components/ErrPopup.js
@@ -1,19 +1,14 @@
 import './ErrPopup.css'
-import {useNavigate} from "react-router-dom";
 
-function ErrPopup({trigger, setTrigger, status, message, dest}) {
-    const navigate = useNavigate();
+function ErrPopup({trigger, setTrigger, status, message}) {
     return (trigger) ? (<div className="popup">
-            <div className="popup-inner">
-                <h3>Error {status}</h3>
-                <p>{message}</p>
-                <button className="close-btn" onClick={() => {
-                    setTrigger(false);
-                    navigate(dest);
-                }}>Received
-                </button>
-            </div>
-        </div>) : "";
+        <div className="popup-inner">
+            <h3>Error {status}</h3>
+            <p>{message}</p>
+            <button className="close-btn" onClick={() => setTrigger(false)}>Received
+            </button>
+        </div>
+    </div>) : "";
 }
 
 export default ErrPopup;
