11b92f711b65204fec902bde18f9250451e13af6
NachitoPerez <irperez@fi.uba.ar>
2022-05-21 21:36:34 -0300

    feat: added back button to return from users to dashboard

--- web/src/components/Users/UsersPage.js
+++ web/src/components/Users/UsersPage.js
@@ -1,77 +1,83 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
 import UsersTable from './UsersTable';
 import StatusBar from '../StatusBar/StatusBar';
-import { blockUser, unblockUser, getUsers } from '../../utils/api/usersApi';
-import NavBar from "../NavBar";
+import {blockUser, unblockUser, getUsers} from '../../utils/api/usersApi';
+import {Button, Divider, Toolbar} from "@mui/material";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import {useNavigate} from "react-router-dom";
 
-export default function UsersPage({setToken}) {
-  const [users, setUsers] = useState([]);
-  const [apiActions, setApiAction] = useState([]);
+export default function UsersPage() {
+    const [users, setUsers] = useState([]);
+    const [apiActions, setApiAction] = useState([]);
+    const navigate = useNavigate();
 
-  async function fetchUsers() {
-    const apiResponse = await getUsers();
+    async function fetchUsers() {
+        const apiResponse = await getUsers();
 
-    setUsers(apiResponse.data.users);
-  }
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const handleBlockUser = async (id) => {
-    const response = await blockUser(id);
-
-    if (response.ok) {
-      setUsers(
-        users.map((user) => {
-          if (user._id === id) {
-            user.disabled = true;
-          }
-
-          return user;
-        })
-      );
-      handleApiAction('ok', `Succesfully blocked user ${id}`);
-    } else {
-      handleApiAction('error', `Error when trying to block user ${id}`);
+        setUsers(apiResponse.data.users);
     }
-  };
 
-  const handleUnblockUser = async (id) => {
-    const response = await unblockUser(id);
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-    if (response.ok) {
-      setUsers(
-        users.map((user) => {
-          if (user._id === id) {
-            user.disabled = false;
-          }
+    const handleBlockUser = async (id) => {
+        const response = await blockUser(id);
 
-          return user;
-        })
-      );
-      handleApiAction('ok', `Succesfully unblocked user ${id}`);
-    } else {
-      handleApiAction('error', `Error when trying to unblock user ${id}`);
-    }
-  };
+        if (response.ok) {
+            setUsers(
+                users.map((user) => {
+                    if (user._id === id) {
+                        user.disabled = true;
+                    }
 
-  const handleApiAction = (type, message) => {
-    setApiAction([{ type: type, msg: message }]);
-  };
+                    return user;
+                })
+            );
+            handleApiAction('ok', `Succesfully blocked user ${id}`);
+        } else {
+            handleApiAction('error', `Error when trying to block user ${id}`);
+        }
+    };
 
-  return (
-    <div>
-      <div>
-        <NavBar setToken={setToken}/>
-      </div>
-      <StatusBar actions={apiActions} />
-      <UsersTable
-        users={users}
-        handleBlockUser={handleBlockUser}
-        handleUnblockUser={handleUnblockUser}
-      />
-    </div>
-  );
+    const handleUnblockUser = async (id) => {
+        const response = await unblockUser(id);
+
+        if (response.ok) {
+            setUsers(
+                users.map((user) => {
+                    if (user._id === id) {
+                        user.disabled = false;
+                    }
+
+                    return user;
+                })
+            );
+            handleApiAction('ok', `Succesfully unblocked user ${id}`);
+        } else {
+            handleApiAction('error', `Error when trying to unblock user ${id}`);
+        }
+    };
+
+    const handleApiAction = (type, message) => {
+        setApiAction([{type: type, msg: message}]);
+    };
+
+    return (
+        <div>
+            <div>
+                <Toolbar>
+                    <Button onClick={() => navigate("/dashboard")} startIcon={<ArrowBackIosIcon/>}></Button>
+                </Toolbar>
+            </div>
+            <Divider/>
+            <StatusBar actions={apiActions}/>
+            <UsersTable
+                users={users}
+                handleBlockUser={handleBlockUser}
+                handleUnblockUser={handleUnblockUser}
+            />
+        </div>
+    );
 }
