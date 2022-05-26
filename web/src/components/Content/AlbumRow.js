71847f0f83713bb8fd0ea09b556a678550df9224
NachitoPerez <irperez@fi.uba.ar>
2022-05-25 22:18:50 -0300

    feat: album-title clicked shows tracks

--- web/src/components/Content/AlbumRow.js
+++ web/src/components/Content/AlbumRow.js
@@ -2,37 +2,32 @@ import {TableCell, TableRow} from "@mui/material";
 import PropTypes from "prop-types";
 
 export default function AlbumRow({album, turnOnPopup, setAlbum2Show}) {
-    const {_id, title, tracks, artist_id, tier} = album;
+    const {title, artistId, tier} = album;
     //mostrar tracks como popup al clickear el title
     return (<TableRow>
             <TableCell><p onClick={() => {
+                setAlbum2Show(album);
                 turnOnPopup(true);
-                if (tracks) {
-                    setAlbum2Show(album);
-                }
-            }}>{_id}</p></TableCell>
-            <TableCell>{title}</TableCell>
-            <TableCell>{artist_id}</TableCell>
+            }}>{title}</p></TableCell>
+            <TableCell>{artistId}</TableCell>
             <TableCell>{tier}</TableCell>
         </TableRow>);
 }
 
 AlbumRow.propTypes = {
     album: PropTypes.shape({
-        _id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
         tracks: PropTypes.arrayOf(PropTypes.shape({
-            _id: PropTypes.string.isRequired,
             title: PropTypes.string.isRequired,
-            artist_id: PropTypes.any.isRequired,
-            album_id: PropTypes.any.isRequired,
+            artistId: PropTypes.string.isRequired,
+            albumId: PropTypes.string.isRequired,
             duration: PropTypes.number.isRequired,
             url: PropTypes.string.isRequired,
             tier: PropTypes.string.isRequired,
             genre: PropTypes.string.isRequired,
             description: PropTypes.string,
         })),
-        artist_id: PropTypes.any.isRequired,
+        artistId: PropTypes.string.isRequired,
         tier: PropTypes.string.isRequired,
     }).isRequired,
 };
