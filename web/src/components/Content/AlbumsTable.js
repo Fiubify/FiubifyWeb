9c2da48c4a8b4c9d8dad45e6681d6473c4631c5d
NachitoPerez <irperez@fi.uba.ar>
2022-05-25 22:16:21 -0300

    feat: getAlbums used

--- web/src/components/Content/AlbumsTable.js
+++ web/src/components/Content/AlbumsTable.js
@@ -1,12 +1,23 @@
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import PropTypes from "prop-types";
 import AlbumRow from "./AlbumRow";
-import {useState} from "@types/react";
+import {useState, useEffect} from "react";
 import AlbumTracksPopup from "../Popup/AlbumTracksPopup";
+import {getAlbums} from "../../utils/api/contentApi";
 
-export default function AlbumsTable({content}) {
+export default function AlbumsTable() {
     const [popupOn, setPopupOn] = useState(false);
     const [album2Show, setAlbum2Show] = useState(null);
+
+    const [content, setContent] = useState([]);
+    async function fetchContent() {
+        const apiResponse = await getAlbums();
+        setContent(apiResponse.data);
+    }
+
+    useEffect(() => {
+        fetchContent();
+    }, []);
 
     return (<div>
         <div>
@@ -21,7 +32,8 @@ export default function AlbumsTable({content}) {
                     </TableHead>
                     <TableBody>
                         {content.map((album) => (<AlbumRow
-                            key={album._id}
+                            //key={album._id}
+                            key={album.title}
                             album={album}
                             turnOnPopup={setPopupOn}
                             setAlbum2Show={setAlbum2Show}
@@ -36,20 +48,18 @@ export default function AlbumsTable({content}) {
 
 AlbumsTable.propTypes = {
     content: PropTypes.arrayOf(PropTypes.shape({
-        _id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
         tracks: PropTypes.arrayOf(PropTypes.shape({
-            _id: PropTypes.string.isRequired,
             title: PropTypes.string.isRequired,
-            artist_id: PropTypes.any.isRequired,
-            album_id: PropTypes.any.isRequired,
+            artistId: PropTypes.string.isRequired,
+            albumId: PropTypes.string.isRequired,
             duration: PropTypes.number.isRequired,
             url: PropTypes.string.isRequired,
             tier: PropTypes.string.isRequired,
             genre: PropTypes.string.isRequired,
             description: PropTypes.string,
         })),
-        artist_id: PropTypes.any.isRequired,
+        artistId: PropTypes.string.isRequired,
         tier: PropTypes.string.isRequired,
     }).isRequired).isRequired,
 };
