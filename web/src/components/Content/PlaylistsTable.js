09ccd3dc6373a50172de8bc839ab35ea4332a882
NachitoPerez <irperez@fi.uba.ar>
2022-06-04 17:59:40 -0300

    feat: added playlist_visualization

--- web/src/components/Content/PlaylistsTable.js
+++ web/src/components/Content/PlaylistsTable.js
@@ -1,22 +1,24 @@
 import {useEffect, useState} from "react";
 import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
-import {playlistsData} from "../../utils/playlistData";
 import PlaylistRow from "./PlaylistRow";
 import PlaylistPopup from "../Popup/PlaylistPopup";
+import {getPlaylists} from "../../utils/api/contentApi";
+import PropTypes from "prop-types";
 
 export default function PlaylistsTable() {
     const [popupOn, setPopupOn] = useState(false);
     const [playlist2Show, setPlaylist2Show] = useState(null);
 
-    /*const [content, setContent] = useState([]);
+    const [content, setContent] = useState([]);
     async function fetchContent() {
-        //const apiResponse = await getPlaylists();
-        //setContent(apiResponse.data);
+        const apiResponse = await getPlaylists();
+        console.log(apiResponse.data);
+        setContent(apiResponse.data);
     }
 
     useEffect(() => {
         fetchContent();
-    }, []);*/
+    }, []);
 
     return (<div>
         <div>
@@ -28,18 +30,31 @@ export default function PlaylistsTable() {
                             <TableCell>Collaborative</TableCell>
                         </TableRow>
                     </TableHead>
-                    <TableBody>
-                        {playlistsData.map((playlist,key) => (<PlaylistRow
-                            key={key}
+                    {<TableBody>
+                        {content.map((playlist) => (<PlaylistRow
+                            key={playlist._id}
                             //key={playlist.title}
                             playlist={playlist}
                             turnOnPopup={setPopupOn}
                             setPlaylist2Show={setPlaylist2Show}
                         />))}
-                    </TableBody>
+                    </TableBody>}
                 </Table>
             </TableContainer>
         </div>
         <PlaylistPopup trigger={popupOn} setTrigger={setPopupOn} playlist={playlist2Show}></PlaylistPopup>
     </div>);
+}
+
+PlaylistsTable.propTypes = {
+    content: PropTypes.arrayOf(PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        owners: PropTypes.arrayOf(PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            id: PropTypes.string,
+        })),
+        collaborative: PropTypes.bool.isRequired,
+    }).isRequired),
 }
