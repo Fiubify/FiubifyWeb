1d6f6d218278d83f62909a9ea9a01c924a502fd9
NachitoPerez <irperez@fi.uba.ar>
2022-06-04 18:00:28 -0300

    temp: added fetch tracks from a playlist

--- web/src/components/Popup/TracksWrapper.js
+++ web/src/components/Popup/TracksWrapper.js
@@ -1,27 +1,46 @@
 import PropTypes from "prop-types";
+import {useEffect, useState} from "react";
+import {getTracksFromPlaylist} from "../../utils/api/contentApi";
 
-export default function TracksWrapper({tracks}) {
-    return (tracks) ? (
-        <div>
-            {tracks.map((track) => (<p>
-                {track.title}
-            </p>))}
-        </div>
-    ) : (<div>
-        <p>No songs available</p>
-    </div>);
+export default function TracksWrapper({playlist_id}) {
+    const [content, setContent] = useState([]);
+    async function fetchContent() {
+        const apiResponse = await getTracksFromPlaylist(playlist_id);
+        console.log(apiResponse.data);
+        setContent(apiResponse.data);
+    }
+
+    useEffect(() => {
+        fetchContent();
+    }, []);
+
+    return(<div>
+        {content.tracks?.map((track) => (<p>
+            {track.title}
+        </p>))}
+    </div>)
 }
 
 TracksWrapper.propTypes = {
-    tracks: PropTypes.arrayOf(PropTypes.shape({
+    content: PropTypes.arrayOf(PropTypes.shape({
         _id: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
-        artist_id: PropTypes.any.isRequired,
-        album_id: PropTypes.any.isRequired,
-        duration: PropTypes.number.isRequired,
-        url: PropTypes.string.isRequired,
-        tier: PropTypes.string.isRequired,
-        genre: PropTypes.string.isRequired,
         description: PropTypes.string,
-    })),
+        owners: PropTypes.arrayOf(PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            id: PropTypes.string,
+        })),
+        collaborative: PropTypes.bool.isRequired,
+        tracks: PropTypes.arrayOf(PropTypes.shape({
+            _id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            artistId: PropTypes.string.isRequired,
+            albumId: PropTypes.string.isRequired,
+            duration: PropTypes.number.isRequired,
+            url: PropTypes.string.isRequired,
+            tier: PropTypes.string.isRequired,
+            genre: PropTypes.string.isRequired,
+            description: PropTypes.string,
+        })).isRequired,
+    }).isRequired),
 };
