d9bf931f6207bd7db97b825ebf789ec684b159b8
NachitoPerez <irperez@fi.uba.ar>
2022-06-12 16:47:00 -0300

    feat: added table_column genre

--- web/src/components/Content/AlbumRow.js
+++ web/src/components/Content/AlbumRow.js
@@ -2,33 +2,24 @@ import {Chip, TableCell, TableRow} from "@mui/material";
 import PropTypes from "prop-types";
 
 export default function AlbumRow({album, turnOnPopup, setAlbum2Show}) {
-    const {title, artistId, tier} = album;
+    const {title, artistId, tier, genre} = album;
 
     return (<TableRow>
         <TableCell><Chip label={title} variant="outlined" onClick={() => {
             setAlbum2Show(album);
             turnOnPopup(true);
-        }}>{title}</Chip></TableCell>
+        }}></Chip></TableCell>
         <TableCell>{artistId}</TableCell>
         <TableCell>{tier}</TableCell>
+        <TableCell>{genre}</TableCell>
     </TableRow>);
 }
 
 AlbumRow.propTypes = {
     album: PropTypes.shape({
         title: PropTypes.string.isRequired,
-        tracks: PropTypes.arrayOf(PropTypes.shape({
-            _id: PropTypes.string.isRequired,
-            title: PropTypes.string.isRequired,
-            artistId: PropTypes.string.isRequired,
-            albumId: PropTypes.string.isRequired,
-            duration: PropTypes.number.isRequired,
-            url: PropTypes.string.isRequired,
-            tier: PropTypes.string.isRequired,
-            genre: PropTypes.string.isRequired,
-            description: PropTypes.string,
-        })),
         artistId: PropTypes.string.isRequired,
         tier: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
     }).isRequired,
 };
