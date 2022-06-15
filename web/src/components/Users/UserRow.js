b19da0002846b1f0eef7a398f2384368a07488b4
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-15 13:34:16 -0300

    se agrega style de users

--- web/src/components/Users/UserRow.js
+++ web/src/components/Users/UserRow.js
@@ -1,50 +1,66 @@
-import {TableCell, TableRow, Chip} from '@mui/material';
-import ButtonWithLoading from '../Buttons/ButtonWithLoading';
+import { TableCell, TableRow, Chip } from "@mui/material";
+import ButtonWithLoading from "../Buttons/ButtonWithLoading";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default function UserRow({user, handleBlockUser, handleUnblockUser, turnOnPopup, setUserProfile}) {
-    const {_id, uid, email, role, disabled} = user;
-    const buttonText = disabled ? 'Unblock' : 'Block';
-    const buttonAction = disabled ? handleUnblockUser : handleBlockUser;
+export default function UserRow({
+  user,
+  handleBlockUser,
+  handleUnblockUser,
+  turnOnPopup,
+  setUserProfile,
+}) {
+  const { _id, uid, email, role, disabled } = user;
+  const buttonText = disabled ? "Unblock" : "Block";
+  const buttonAction = disabled ? handleUnblockUser : handleBlockUser;
 
-    return (
-        <TableRow>
-            <TableCell><p onClick={() => {
-                turnOnPopup(true);
-                setUserProfile(user);
-            }}>{_id}</p></TableCell>
-            <TableCell>{uid}</TableCell>
-            <TableCell>{email}</TableCell>
-            <TableCell>
-                <Chip color='info' label={role}></Chip>
-            </TableCell>
-            <TableCell role={'status'}>
-                <Chip
-                    color={disabled ? 'error' : 'success'}
-                    label={String(disabled).toUpperCase()}
-                />
-            </TableCell>
-            <TableCell align='center'>
-                <ButtonWithLoading
-                    variant={'contained'}
-                    onClickHandler={() => buttonAction(_id)}
-                >
-                    {buttonText}
-                </ButtonWithLoading>
-            </TableCell>
-        </TableRow>
-    );
+  return (
+    <TableRow>
+      <TableCell sx={{ borderBottom: "2px solid #006e95" }} color="info">
+        <p
+          onClick={() => {
+            turnOnPopup(true);
+            setUserProfile(user);
+          }}
+        >
+          {_id}
+        </p>
+      </TableCell>
+      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>{uid}</TableCell>
+      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>{email}</TableCell>
+      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>
+        <Chip
+          sx={{ backgroundColor: "#006e95" }}
+          color="info"
+          label={role}
+        ></Chip>
+      </TableCell>
+      <TableCell sx={{ borderBottom: "2px solid #006e95" }} role={"status"}>
+        <Chip
+          color={disabled ? "error" : "success"}
+          label={String(disabled).toUpperCase()}
+        />
+      </TableCell>
+      <TableCell sx={{ borderBottom: "2px solid #006e95" }} align="center">
+        <ButtonWithLoading
+          variant={"contained"}
+          onClickHandler={() => buttonAction(_id)}
+        >
+          {buttonText}
+        </ButtonWithLoading>
+      </TableCell>
+    </TableRow>
+  );
 }
 
 UserRow.propTypes = {
-    user: PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        uid: PropTypes.string.isRequired,
-        email: PropTypes.string.isRequired,
-        role: PropTypes.string.isRequired,
-        disabled: PropTypes.bool.isRequired,
-    }).isRequired,
-    handleBlockUser: PropTypes.func.isRequired,
-    handleUnblockUser: PropTypes.func.isRequired,
+  user: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    uid: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+    disabled: PropTypes.bool.isRequired,
+  }).isRequired,
+  handleBlockUser: PropTypes.func.isRequired,
+  handleUnblockUser: PropTypes.func.isRequired,
 };
