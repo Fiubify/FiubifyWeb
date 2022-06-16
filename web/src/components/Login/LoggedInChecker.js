9194f4c0c25bfb3285fceeacf54b86047951b05a
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-16 00:46:17 -0300

    se estiliza page does not exist

--- web/src/components/Login/LoggedInChecker.js
+++ web/src/components/Login/LoggedInChecker.js
@@ -1,11 +1,22 @@
-function LoggedInChecker({component, token}) {
-    if (token !== '') {
-        return <div>{component}</div>
-    } else {
-        return <div>
-            <h2>Page does not exist</h2>
-        </div>
-    }
+function LoggedInChecker({ component, token }) {
+  if (token !== "") {
+    return <div>{component}</div>;
+  } else {
+    return (
+      <div
+        style={{
+          color: "#006e95",
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h2>Page does not exist</h2>
+      </div>
+    );
+  }
 }
 
 export default LoggedInChecker;
