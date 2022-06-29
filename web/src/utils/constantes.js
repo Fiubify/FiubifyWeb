13164945b04f4fa326243c8757884c9ff7879592
NachitoPerez <irperez@fi.uba.ar>
2022-06-28 22:34:57 -0300

    feat: added file with constants for url (BASE & metrics)

---
+++ web/src/utils/constantes.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://fiubify-middleware-staging.herokuapp.com";
+
+export const userMetricsUrl = `${BASE_URL}/metrics/users/events`;
+export const contentMetricsUrl = `${BASE_URL}/metrics/contents/events`;
