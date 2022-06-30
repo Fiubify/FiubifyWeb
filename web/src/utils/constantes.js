f34b41c8246803190f113b12e820206af31781b9
NachitoPerez <irperez@fi.uba.ar>
2022-06-30 17:20:19 -0300

    refactor

--- web/src/utils/constantes.js
+++ web/src/utils/constantes.js
@@ -2,3 +2,6 @@ export const BASE_URL = "https://fiubify-middleware-staging.herokuapp.com";
 
 export const userMetricsUrl = `${BASE_URL}/metrics/users/events`;
 export const contentMetricsUrl = `${BASE_URL}/metrics/contents/events`;
+
+export const creationContentAction = "Creation";
+export const listenedContentAction = "Listened";
