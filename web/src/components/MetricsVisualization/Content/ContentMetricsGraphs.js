5178a1fa65f2babc408c12b8f84747763931f432
NachitoPerez <irperez@fi.uba.ar>
2022-07-04 12:52:36 -0300

    refactor

--- web/src/components/MetricsVisualization/Content/ContentMetricsGraphs.js
+++ web/src/components/MetricsVisualization/Content/ContentMetricsGraphs.js
@@ -7,23 +7,11 @@ import {TopSongsChart} from "./TopSongsChart";
 import {TopAlbumsChart} from "./TopAlbumsChart";
 import {TopGenresChart} from "./TopGenresChart";
 
-function getCurrentDate(separator='-'){
-
-    let newDate = new Date()
-    // let date = newDate.getDate();
-    // let month = newDate.getMonth() + 1;
-    // let year = newDate.getFullYear();
-    //
-    //return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
-    return newDate.toUTCString();
-}
-
 export function ContentMetricsGraphs() {
     const [content, setContent] = useState([]);
     useEffect(() => {
         getAllContentMetrics().then((contents) => setContent(contents.data));
     }, []);
-
 
     if (content.length > 0) {
         return (<div>
@@ -39,32 +27,6 @@ export function ContentMetricsGraphs() {
             <TopAlbumsChart/>
             <h2>Top Genres listened</h2>
             <TopGenresChart/>
-            {/*<VictoryChart
-                // adding the material theme provided with Victory
-                theme={VictoryTheme.material}
-                domainPadding={20}
-                height={200}
-                width={300}
-            >
-                <VictoryAxis
-                    tickValues={[1, 2, 3, 4]}
-                    tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-                />
-                <VictoryAxis
-                    dependentAxis
-                    tickFormat={(x) => (`$${x / 1000}k`)}
-                />
-                <VictoryBar
-                    name={"Last Week ContentMetrics"}
-                    data={data}
-                    x="quarter"
-                    y="earnings"
-                    animate={{
-                        duration: 2000,
-                        onLoad: {duration: 1000}
-                    }}
-                />
-            </VictoryChart>*/}
         </div>);
     } else {
         return "";
