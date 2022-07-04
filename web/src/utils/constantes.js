ca3177e4832bdef6c59f63fe3bf2f78b4fe683e3
NachitoPerez <irperez@fi.uba.ar>
2022-07-04 12:17:39 -0300

    feat: added Tops urls as constants

--- web/src/utils/constantes.js
+++ web/src/utils/constantes.js
@@ -6,8 +6,11 @@ export const signupUserAction = "Signup";
 export const emailUserType = "Email";
 export const federatedUserType = "Federated";
 
-
 export const contentMetricsUrl = `${BASE_URL}/metrics/contents/events`;
 export const creationContentAction = "Creation";
 export const listenedContentAction = "Listened";
 export const freeTier = "Free";
+
+export const topSongsUrl = `${BASE_URL}/metrics/contents/events/agg_by_song`;
+export const topAlbumsUrl = `${BASE_URL}/metrics/contents/events/agg_by_album`;
+export const topGenresUrl = `${BASE_URL}/metrics/contents/events/agg_by_genre`;
