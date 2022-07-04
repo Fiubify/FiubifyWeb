b6269774cdd6e0b2cc6dcdcca3d7af39aa04e043
NachitoPerez <irperez@fi.uba.ar>
2022-07-03 21:04:32 -0300

    feat: added constants for userMetric's actions and types

--- web/src/utils/constantes.js
+++ web/src/utils/constantes.js
@@ -1,8 +1,13 @@
 export const BASE_URL = "https://fiubify-middleware-staging.herokuapp.com";
 
 export const userMetricsUrl = `${BASE_URL}/metrics/users/events`;
-export const contentMetricsUrl = `${BASE_URL}/metrics/contents/events`;
+export const loginUserAction = "Login";
+export const signupUserAction = "Signup";
+export const emailUserType = "Email";
+export const federatedUserType = "Federated";
 
+
+export const contentMetricsUrl = `${BASE_URL}/metrics/contents/events`;
 export const creationContentAction = "Creation";
 export const listenedContentAction = "Listened";
 export const freeTier = "Free";
