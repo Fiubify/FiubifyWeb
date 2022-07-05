f7383cf0e7744c7940a5db083efeb00a853098bb
NachitoPerez <irperez@fi.uba.ar>
2022-07-04 23:05:57 -0300

    feat: added button to block/unblock songs

--- web/src/components/Content/SongRow.js
+++ web/src/components/Content/SongRow.js
@@ -1,49 +1,70 @@
-import { Button, TableCell, TableRow } from "@mui/material";
+import {Button, Chip, TableCell, TableRow} from "@mui/material";
 import PropTypes from "prop-types";
-import { BiWindowOpen } from "react-icons/bi";
+import {BiWindowOpen} from "react-icons/bi";
+import ButtonWithLoading from "../Buttons/ButtonWithLoading";
 
-export default function SongRow({ song, turnOnPopup, setSongDescription }) {
-  const { title, artistId, albumId, duration, url, tier, genre } = song;
+export default function SongRow({song, turnOnPopup, setSongDescription, handleBlockSong, handleUnblockSong}) {
+    const { _id, title, artistId, albumId, duration, url, tier, genre, disabled} = song;
+    const buttonText = disabled ? "Unblock" : "Block";
+    const buttonAction = disabled ? handleUnblockSong : handleBlockSong;
 
-  return (
-    <TableRow>
-      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>
-        <Button
-          onClick={() => {
-            setSongDescription(song);
-            turnOnPopup(true);
-          }}
-          sx={{ color: "black" }}
-        >
-          <BiWindowOpen />
-        </Button>
-      </TableCell>
-      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>{title}</TableCell>
-      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>
-        {artistId}
-      </TableCell>
-      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>
-        {albumId}
-      </TableCell>
-      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>
-        {duration}
-      </TableCell>
-      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>{url}</TableCell>
-      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>{tier}</TableCell>
-      <TableCell sx={{ borderBottom: "2px solid #006e95" }}>{genre}</TableCell>
-    </TableRow>
-  );
+    return (
+        <TableRow>
+            <TableCell sx={{borderBottom: "2px solid #006e95"}}>
+                <Button
+                    onClick={() => {
+                        setSongDescription(song);
+                        turnOnPopup(true);
+                    }}
+                    sx={{color: "black"}}
+                >
+                    <BiWindowOpen/>
+                </Button>
+            </TableCell>
+            <TableCell sx={{borderBottom: "2px solid #006e95"}}>{title}</TableCell>
+            <TableCell sx={{borderBottom: "2px solid #006e95"}}>
+                {artistId}
+            </TableCell>
+            <TableCell sx={{borderBottom: "2px solid #006e95"}}>
+                {albumId}
+            </TableCell>
+            <TableCell sx={{borderBottom: "2px solid #006e95"}}>
+                {duration}
+            </TableCell>
+            <TableCell sx={{borderBottom: "2px solid #006e95"}}>{url}</TableCell>
+            <TableCell sx={{borderBottom: "2px solid #006e95"}}>{tier}</TableCell>
+            <TableCell sx={{borderBottom: "2px solid #006e95"}}>{genre}</TableCell>
+            <TableCell sx={{ borderBottom: "2px solid #006e95" }} role={"status"}>
+                <Chip
+                    color={disabled ? "error" : "success"}
+                    label={String(disabled).toUpperCase()}
+                />
+            </TableCell>
+            <TableCell sx={{ borderBottom: "2px solid #006e95" }} align="center">
+                <ButtonWithLoading
+                    variant={"contained"}
+                    onClickHandler={() => buttonAction(_id)}
+                >
+                    {buttonText}
+                </ButtonWithLoading>
+            </TableCell>
+        </TableRow>
+    );
 }
 
 SongRow.propTypes = {
-  song: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    artistId: PropTypes.string.isRequired,
-    albumId: PropTypes.string.isRequired,
-    duration: PropTypes.number.isRequired,
-    url: PropTypes.string.isRequired,
-    tier: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-    description: PropTypes.string,
-  }).isRequired,
+    song: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        artistId: PropTypes.string.isRequired,
+        albumId: PropTypes.string.isRequired,
+        duration: PropTypes.number.isRequired,
+        url: PropTypes.string.isRequired,
+        tier: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        disabled: PropTypes.bool.isRequired,
+    }).isRequired,
+    handleBlockSong: PropTypes.func.isRequired,
+    handleUnblockSong: PropTypes.func.isRequired,
 };
--- web/src/components/Content/SongsTable.js
+++ web/src/components/Content/SongsTable.js
@@ -1,81 +1,133 @@
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
 } from "@mui/material";
 import SongRow from "./SongRow";
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import SongDescriptionPopup from "../Popup/Songs/SongDescriptionPopup";
-import { getSongs } from "../../utils/api/contentApi";
+import {blockSong, getSongs, unblockSong} from "../../utils/api/contentApi";
+import StatusBar from "../StatusBar/StatusBar";
 
-export default function SongsTable() {
-  const [popupOn, setPopupOn] = useState(false);
-  const [song2Describe, setSong2Describe] = useState(null);
+export default function SongsTable({token}) {
+    const [popupOn, setPopupOn] = useState(false);
+    const [song2Describe, setSong2Describe] = useState(null);
+    const [apiActions, setApiAction] = useState([]);
 
-  const [content, setContent] = useState([]);
-  async function fetchContent() {
-    const apiResponse = await getSongs();
-    setContent(apiResponse.data);
-  }
+    const [content, setContent] = useState([]);
 
-  useEffect(() => {
-    fetchContent();
-  }, []);
+    async function fetchContent() {
+        const apiResponse = await getSongs();
+        setContent(apiResponse.data);
+    }
 
-  return (
-    <div style={{ width: "100%" }}>
-      <div style={{ width: "100%" }}>
-        <TableContainer sx={{ width: "100%" }}>
-          <Table>
-            <TableHead sx={{ backgroundColor: "#006e95" }}>
-              <TableRow>
-                <TableCell sx={{ color: "white" }}>View more</TableCell>
-                <TableCell sx={{ color: "white" }}>Title</TableCell>
-                <TableCell sx={{ color: "white" }}>Artist_ID</TableCell>
-                <TableCell sx={{ color: "white" }}>Album_ID</TableCell>
-                <TableCell sx={{ color: "white" }}>Duration</TableCell>
-                <TableCell sx={{ color: "white" }}>URL</TableCell>
-                <TableCell sx={{ color: "white" }}>Tier</TableCell>
-                <TableCell sx={{ color: "white" }}>Genre</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {content.map((song) => (
-                <SongRow
-                  key={song.url}
-                  song={song}
-                  turnOnPopup={setPopupOn}
-                  setSongDescription={setSong2Describe}
-                />
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-      <SongDescriptionPopup
-        trigger={popupOn}
-        setTrigger={setPopupOn}
-        song={song2Describe}
-      ></SongDescriptionPopup>
-    </div>
-  );
+    useEffect(() => {
+        fetchContent().then();
+    }, []);
+
+    const handleBlockSong = async (id) => {
+        const response = await blockSong(id, token);
+
+        if (response.ok) {
+            setContent(
+                content.map((song) => {
+                    if (song._id === id) {
+                        song.disabled = true;
+                    }
+
+                    return song;
+                })
+            );
+            handleApiAction("ok", `Successfully blocked song ${id}`);
+        } else {
+            handleApiAction("error", `Error when trying to block song ${id}`);
+        }
+    };
+
+    const handleUnblockSong = async (id) => {
+        const response = await unblockSong(id, token);
+
+        if (response.ok) {
+            setContent(
+                content.map((song) => {
+                    if (song._id === id) {
+                        song.disabled = false;
+                    }
+
+                    return song;
+                })
+            );
+            handleApiAction("ok", `Successfully unblocked song ${id}`);
+        } else {
+            handleApiAction("error", `Error when trying to unblock song ${id}`);
+        }
+    };
+
+    const handleApiAction = (type, message) => {
+        setApiAction([{type: type, msg: message}]);
+    };
+
+    return (
+        <div style={{width: "100%"}}>
+            <StatusBar actions={apiActions} />
+            <div style={{width: "100%"}}>
+                <TableContainer sx={{width: "100%"}}>
+                    <Table>
+                        <TableHead sx={{backgroundColor: "#006e95"}}>
+                            <TableRow>
+                                <TableCell sx={{color: "white"}}>View more</TableCell>
+                                <TableCell sx={{color: "white"}}>Title</TableCell>
+                                <TableCell sx={{color: "white"}}>Artist_ID</TableCell>
+                                <TableCell sx={{color: "white"}}>Album_ID</TableCell>
+                                <TableCell sx={{color: "white"}}>Duration</TableCell>
+                                <TableCell sx={{color: "white"}}>URL</TableCell>
+                                <TableCell sx={{color: "white"}}>Tier</TableCell>
+                                <TableCell sx={{color: "white"}}>Genre</TableCell>
+                                <TableCell sx={{ color: "white" }}>Blocked?</TableCell>
+                                <TableCell sx={{ color: "white" }} align="center">
+                                    Operation
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {content.map((song) => (
+                                <SongRow
+                                    key={song.url}
+                                    song={song}
+                                    turnOnPopup={setPopupOn}
+                                    setSongDescription={setSong2Describe}
+                                    handleBlockSong={handleBlockSong}
+                                    handleUnblockSong={handleUnblockSong}
+                                />
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+            <SongDescriptionPopup
+                trigger={popupOn}
+                setTrigger={setPopupOn}
+                song={song2Describe}
+            ></SongDescriptionPopup>
+        </div>
+    );
 }
 
 SongsTable.propTypes = {
-  content: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      artistId: PropTypes.string.isRequired,
-      albumId: PropTypes.string.isRequired,
-      duration: PropTypes.number.isRequired,
-      url: PropTypes.string.isRequired,
-      tier: PropTypes.string.isRequired,
-      genre: PropTypes.string.isRequired,
-      description: PropTypes.string,
-    }).isRequired
-  ),
+    content: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            artistId: PropTypes.string.isRequired,
+            albumId: PropTypes.string.isRequired,
+            duration: PropTypes.number.isRequired,
+            url: PropTypes.string.isRequired,
+            tier: PropTypes.string.isRequired,
+            genre: PropTypes.string.isRequired,
+            description: PropTypes.string,
+        }).isRequired
+    ),
 };
